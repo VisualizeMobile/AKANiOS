dd95a216f64a671732babb8ed9cdf9f6e72023ef
ArthurJahn <stutrzbecher@gmail.com>
2014-08-05 14:44:44 -0300

    Adicionando Controller de detalhamento dos gastos

---
+++ AKANiOS/AKANiOS/AKDetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  AKDetailViewController.m
+//  AKANiOS
+//
+//  Created by Arthur Sturzbecher on 04/08/14.
+//  Copyright (c) 2014 Arthur Jahn Sturzbecher. All rights reserved.
+//
+
+#import "AKDetailViewController.h"
+
+@interface AKDetailViewController ()
+
+@end
+
+@implementation AKDetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
--- AKANiOS/AKANiOS/AKTableViewCell.m
+++ AKANiOS/AKANiOS/AKTableViewCell.m
@@ -17,6 +17,9 @@
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated
 {
-    [super setSelected:selected animated:animated];
+    //[super setSelected:selected animated:animated];
+    if (selected) {
+        
+    }
 }
 @end
